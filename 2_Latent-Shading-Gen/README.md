13323781bf37e8aa72ecf16bbe444db1700ccb4c
bhushan <bhushan.s.94@gmail.com>
2019-04-15 20:05:27 -0400

    Latent Shading Generation Network

---
+++ 2_Latent-Shading-Gen/README.md
@@ -0,0 +1,2 @@
+## Latent Shading Generation
+In this approach, we avoid generation of Spherical Harmonics and instead generate shading directly using Normal, Albedo and Image features. And then use this latent lighting along with Normal to generate shading
---
+++ 2_Latent-Shading-Gen/models.py
@@ -0,0 +1,216 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Base methods for creating convnet
+def get_conv(in_channels, out_channels, kernel_size=3, padding=0, stride=1, dropout=0):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, 
+                    padding=padding, bias=False),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+        nn.Dropout(dropout)
+    )
+
+# SfSNet Models
+class ResNetBlock(nn.Module):
+    """ Basic building block of ResNet to be used for Normal and Albedo Residual Blocks
+    """
+    def __init__(self, in_planes, out_planes, stride=1):
+        super(ResNetBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_planes)
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+        self.shortcut = nn.Sequential()
+
+    def forward(self, x):
+        out = F.relu(self.bn1(x))
+        out = F.relu(self.bn2(self.conv1(out)))
+        out = self.conv2(out)
+        out += self.shortcut(x)
+        return out
+
+class baseFeaturesExtractions(nn.Module):
+    """ Base Feature extraction
+    """
+    def __init__(self):
+        super(baseFeaturesExtractions, self).__init__()
+        self.conv1 = get_conv(3, 64, kernel_size=7, padding=3)
+        self.conv2 = get_conv(64, 128, kernel_size=3, padding=1)
+        self.conv3 = get_conv(128, 128, kernel_size=3, stride=2, padding=1)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        return out
+
+class NormalResidualBlock(nn.Module):
+    """ Net to general Normal from features
+    """
+    def __init__(self):
+        super(NormalResidualBlock, self).__init__()
+        self.block1 = ResNetBlock(128, 128)
+        self.block2 = ResNetBlock(128, 128)
+        self.block3 = ResNetBlock(128, 128)
+        self.block4 = ResNetBlock(128, 128)
+        self.block5 = ResNetBlock(128, 128)
+        self.bn1    = nn.BatchNorm2d(128)
+    
+    def forward(self, x):
+        out = self.block1(x)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.block4(out)
+        out = self.block5(out)
+        out = F.relu(self.bn1(out))
+        return out
+
+class AlbedoResidualBlock(nn.Module):
+    """ Net to general Albedo from features
+    """
+    def __init__(self):
+        super(AlbedoResidualBlock, self).__init__()
+        self.block1 = ResNetBlock(128, 128)
+        self.block2 = ResNetBlock(128, 128)
+        self.block3 = ResNetBlock(128, 128)
+        self.block4 = ResNetBlock(128, 128)
+        self.block5 = ResNetBlock(128, 128)
+        self.bn1    = nn.BatchNorm2d(128)
+    
+    def forward(self, x):
+        out = self.block1(x)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.block4(out)
+        out = self.block5(out)
+        out = F.relu(self.bn1(out))
+        return out
+
+class NormalGenerationNet(nn.Module):
+    """ Generating Normal
+    """
+    def __init__(self):
+        super(NormalGenerationNet, self).__init__()
+        # self.upsample = nn.UpsamplingBilinear2d(size=(128, 128), scale_factor=2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.conv1    = get_conv(128, 128, kernel_size=1, stride=1)
+        self.conv2    = get_conv(128, 64, kernel_size=3, padding=1)
+        self.conv3    = nn.Conv2d(64, 3, kernel_size=1)
+
+    def forward(self, x):
+        out = self.upsample(x)
+        out = self.conv1(out)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        return out
+
+class AlbedoGenerationNet(nn.Module):
+    """ Generating Albedo
+    """
+    def __init__(self):
+        super(AlbedoGenerationNet, self).__init__()
+        # self.upsample = nn.UpsamplingBilinear2d(size=(128, 128), scale_factor=2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.conv1    = get_conv(128, 128, kernel_size=1, stride=1)
+        self.conv2    = get_conv(128, 64, kernel_size=3, padding=1)
+        self.conv3    = nn.Conv2d(64, 3, kernel_size=1)
+
+    def forward(self, x):
+        out = self.upsample(x)
+        out = self.conv1(out)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        return out
+
+class NeuralLatentLightEstimator(nn.Module):
+    """ Estimate Neural Latent lighting from normal, albedo and conv features
+    """
+    def __init__(self):
+        super(NeuralLatentLightEstimator, self).__init__()
+        self.upsample = nn.UpsamplingBilinear2d(size=(128, 128), scale_factor=None)
+        self.conv1    = get_conv(384, 128, kernel_size=1, stride=1)
+        self.conv2    = get_conv(128, 64, kernel_size=1, stride=1)
+        self.conv3    = get_conv(64, 64, kernel_size=3, padding=1)
+        self.conv4    = nn.Conv2d(64, 3, kernel_size=1)
+
+    def forward(self, x):
+        out = self.upsample(x)
+        out = self.conv1(out)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        out = self.conv4(out)
+        return out
+
+class ShadingGeneration(nn.Module):
+    """ Correct generated shading with neural light
+    """
+    def __init__(self):
+        super(ShadingGeneration, self).__init__()
+        self.conv1 = get_conv(6, 64, kernel_size=1, padding=0)
+        self.conv2 = nn.Conv2d(64, 3, kernel_size=3, padding=1)
+    
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        return out
+
+class ReconstructImage(nn.Module):
+    """ Reconstruct Image from shading and albedo
+    """
+    def __init__(self):
+        super(ReconstructImage, self).__init__()
+    
+    def forward(self, shading, albedo):
+        return shading * albedo
+        
+class SfsNetPipeline(nn.Module):
+    """ SfSNet Pipeline
+    """
+    def __init__(self, conv_model, normal_residual_model, albedo_residual_model,
+                    normal_gen_model, albedo_gen_model, shading_model,
+                    neural_light_model, image_recon_model):
+        super(SfsNetPipeline, self).__init__()
+        self.conv_model = conv_model
+        self.normal_residual_model = normal_residual_model
+        self.albedo_residual_model = albedo_residual_model
+        self.normal_gen_model      = normal_gen_model
+        self.albedo_gen_model      = albedo_gen_model
+        self.shading_model         = shading_model
+        self.neural_light_model    = neural_light_model
+        self.image_recon_model     = image_recon_model
+
+    def get_face(self, sh, normal, albedo):
+        shading = self.shading_model(normal, sh)
+        recon   = self.image_recon_model(shading, albedo)
+        return recon
+
+    def forward(self, face):
+        # Following is training pipeline
+        # 1. Pass Image from Conv Model to extract features
+        out_features = self.conv_model(face)
+
+        # 2 a. Pass Conv features through Normal Residual
+        out_normal_features = self.normal_residual_model(out_features)
+        # 2 b. Pass Conv features through Albedo Residual
+        out_albedo_features = self.albedo_residual_model(out_features)
+
+        # 3 a. Generate Normal
+        predicted_normal = self.normal_gen_model(out_normal_features)
+        # 3 b. Generate Albedo
+        predicted_albedo = self.albedo_gen_model(out_albedo_features)
+        # 3 c. Estimate lighting
+        # First, concat conv, normal and albedo features over channels dimension
+        all_features = torch.cat((out_features, out_normal_features, out_albedo_features), dim=1)
+        # 3 d. Estimate Neural Light for residual correctness
+        predicted_neural_light = self.neural_light_model(all_features)
+
+        # 4. Generate shading
+        normal_nn_light = torch.cat((predicted_normal, predicted_neural_light), dim=1)
+        out_shading = self.shading_model(normal_nn_light)
+
+        # 6. Reconstruction of image
+        out_recon = self.image_recon_model(out_shading, predicted_albedo)
+                    
+        return predicted_normal, predicted_albedo, out_shading, out_recon
