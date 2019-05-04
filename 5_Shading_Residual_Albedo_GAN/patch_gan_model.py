3145b1bcf901a97be200b8d1e1540f9007d26cb1
bhushan <bhushan.s.94@gmail.com>
2019-05-03 18:22:29 -0700

    add standard disc

--- 5_Shading_Residual_Albedo_GAN/patch_gan_model.py
+++ 5_Shading_Residual_Albedo_GAN/patch_gan_model.py
@@ -133,3 +133,31 @@ class Discriminator(nn.Module):
     #     return self.model(img_input)
     def forward(self, img_input):
         return self.model(img_input)
+
+class PT_Tutorial_Discriminator(nn.Module):
+    def __init__(self, ngpu):
+        super(PT_Tutorial_, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.main(input)
